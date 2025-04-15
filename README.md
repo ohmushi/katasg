a3cfbb21fc46d1e5ee295f03a396ca274306f2ba
ohmushi <omnes.theo@gmail.com>
2025-04-15 10:30:05 +0200

    init project

---
+++ README.md
@@ -0,0 +1,23 @@
+# Kata SG
+
+## 🔧 Installation et exécution
+
+Prérequis : 
+* Java 21+
+
+###  Installation
+```bash
+git clone https://github.com/ohmushi/
+cd 
+./mvnw clean install
+```
+
+### Exécution
+
+```bash
+./mvnw exec:java
+```
+
+## 🪶 Auteur
+
+Theo OMNES (@ohmushi)
---
+++ src/main/java/cat/ohmushi/katasg/SpringsgApplication.java
@@ -0,0 +1,13 @@
+package cat.ohmushi.katasg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsgApplication.class, args);
+	}
+
+}
